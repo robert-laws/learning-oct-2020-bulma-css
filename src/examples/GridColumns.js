7c4dc67fe5fbba1b647f5778b3e9b97c015fa5f4
Robert Laws <roblaws@me.com>
2020-10-08 12:47:09 +0300

    add responsive classes for grid and columns

--- src/examples/GridColumns.js
+++ src/examples/GridColumns.js
@@ -5,20 +5,22 @@ export const GridColumns = () => {
   return (
     <section className='section'>
       <div className='container'>
-        <div className='columns is-vcentered'>
-          <div className='column is-3'>
-            <h1 className='is-size-1 title'>Docker</h1>
-            <h2 className='is-size-2 subtitle'>Dark Roast</h2>
+        <div className='columns is-multiline is-vcentered'>
+          <div className='column is-6-tablet is-3-desktop'>
+            <h1 className='is-size-3-mobile is-size-1-desktop title'>Docker</h1>
+            <h2 className='is-size-3-mobile is-size-2-desktop subtitle'>
+              Dark Roast
+            </h2>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam
               ullam eum recusandae sequi nemo, quaerat cupiditate cumque nulla.
               Culpa, reprehenderit.
             </p>
           </div>
-          <div className='column is-5 has-text-centered'>
+          <div className='column is-6-tablet is-5-desktop has-text-centered'>
             <img src={coffee} alt='Coffee Cup' />
           </div>
-          <div className='column is-4'>
+          <div className='column is-12-tablet is-4-desktop'>
             <div className='is-size-4 mb-4'>$10.95</div>
             <p className='mb-4'>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
